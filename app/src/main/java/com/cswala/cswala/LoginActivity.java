49dcdf4a26059d293f1ec9f6bd62556371024e2f
Rituraj Jain <rrjain20122000@gmail.com>
2021-02-06 21:38:17 +0530

    Added Files

--- app/src/main/java/com/cswala/cswala/LoginActivity.java
+++ app/src/main/java/com/cswala/cswala/LoginActivity.java
@@ -48,6 +48,12 @@ public class LoginActivity extends AppCompatActivity {
 
 
     @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finishAffinity();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
--- app/src/main/java/com/cswala/cswala/MainActivity.java
+++ app/src/main/java/com/cswala/cswala/MainActivity.java
@@ -1,14 +1,36 @@
 package com.cswala.cswala;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class MainActivity extends AppCompatActivity {
+    ChipNavigationBar bt;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt = findViewById(R.id.bottom_navigation);
+        bt.setItemSelected(R.id.explore, true);
+
+        TextView tv = findViewById(R.id.logout);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                firebaseAuth.signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+
+            }
+        });
     }
 }
