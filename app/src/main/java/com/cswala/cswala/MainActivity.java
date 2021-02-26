6ab3df1c9fcb90caa435cee956ff2e1e0d48e322
Victor Pozdnyakov <victorius152@gmail.com>
2021-02-26 16:37:19 +0300

    Add explore fragment

--- app/src/main/java/com/cswala/cswala/MainActivity.java
+++ app/src/main/java/com/cswala/cswala/MainActivity.java
@@ -2,15 +2,18 @@ package com.cswala.cswala;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "ExploreTag";
     ChipNavigationBar bt;
     FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
@@ -18,8 +21,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         bt = findViewById(R.id.bottom_navigation);
         bt.setItemSelected(R.id.explore, true);
+        showFragment(ExploreFragment.class);
+
+        bt.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                launchRequiredFragmentById(i);
+            }
+        });
 
         TextView tv = findViewById(R.id.logout);
 
@@ -33,4 +45,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void launchRequiredFragmentById(int id) {
+        switch (id) {
+            case R.id.explore:
+                // start ExploreFragment
+                Log.d(TAG, "onItemSelected: explore clicked!");
+                showFragment(ExploreFragment.class);
+                break;
+            case R.id.account:
+                // start account fragment
+                break;
+            case R.id.saved:
+                // start jobhunt fragment
+                break;
+            case R.id.inbox:
+                // start community fragment
+                break;
+            case R.id.news:
+                // start hackfeed fragment
+                break;
+        }
+    }
+
+    /**
+     * Close previous Fragment and show another Fragment
+     *
+     * @param fragmentClass extending Fragment class that we need to show
+     */
+    private <T extends Fragment> void showFragment(Class<T> fragmentClass) {
+        Log.d(TAG, "startMenuFragment");
+        getSupportFragmentManager().popBackStack(); // remove previous fragment from back stack
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.fragment_container, fragmentClass, null)
+                .addToBackStack(null)
+                .commit();
+
+    }
+
 }
