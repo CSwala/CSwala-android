0de4bcf1e0a3a0ef460d8bc30576de8dc1de5f67
Viral Kumar Maurya <viralkumarmaurya@gmail.com>
2021-03-09 18:33:10 +0530

    Added new community fragment.

--- app/src/main/java/com/cswala/cswala/MainActivity.java
+++ app/src/main/java/com/cswala/cswala/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.cswala.cswala.fragment.CommunityFragment;
 import com.cswala.cswala.fragment.ProfileFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
@@ -27,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.account:
                         Intent intent = new Intent(getApplicationContext(), ProfileFragment.class);
                         startActivity(intent);
+                        break;
+                    case R.id.inbox:
+                        Intent intent1 = new Intent(getApplicationContext(), CommunityFragment.class);
+                        startActivity(intent1);
                         break;
                 }
             }
---
+++ app/src/main/java/com/cswala/cswala/fragment/CommunityFragment.java
@@ -0,0 +1,16 @@
+package com.cswala.cswala.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cswala.cswala.R;
+
+public class CommunityFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community_fragment);
+    }
+}
