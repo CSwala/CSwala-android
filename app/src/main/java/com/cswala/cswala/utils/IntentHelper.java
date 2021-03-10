cb388a3208fe23a27143eea6bd150f1688803c48
KapilYadav-dev <kapil1910086@akgec.ac.in>
2021-03-10 13:56:44 +0530

    Added IntentHelper class with animatoo library ...

---
+++ app/src/main/java/com/cswala/cswala/utils/IntentHelper.java
@@ -0,0 +1,26 @@
+package com.cswala.cswala.utils;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
+import com.cswala.cswala.LoginActivity;
+import com.cswala.cswala.MainActivity;
+
+public class IntentHelper {
+    Context context;
+
+    public IntentHelper(Context context) {
+        this.context = context;
+    }
+
+    public void GoToHome() {
+        context.startActivity(new Intent(context, MainActivity.class));
+        Animatoo.animateSlideLeft(context);
+    }
+    public void GoToLogin() {
+        context.startActivity(new Intent(context, LoginActivity.class));
+        Animatoo.animateSlideLeft(context);
+    }
+
+}
