72e14cc2c75a26ab31a4e491f8c1b406e3a72b63
Gowtham B <63305824+GowthamGoush@users.noreply.github.com>
2021-03-12 14:46:46 +0530

    Add JobHunt Fragment (#66)

---
+++ app/src/main/java/com/cswala/cswala/JobHunt.java
@@ -0,0 +1,20 @@
+package com.cswala.cswala;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class JobHunt extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_job_hunt, container, false);
+        return view;
+    }
+}
