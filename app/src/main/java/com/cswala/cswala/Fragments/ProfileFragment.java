6dbad93cd5e3261194f4a15b78a786ac5b4bf213
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-03-19 22:42:10 +0530

    Relocate logout button

--- app/src/main/java/com/cswala/cswala/Fragments/ProfileFragment.java
+++ app/src/main/java/com/cswala/cswala/Fragments/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.cswala.cswala.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +8,34 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.cswala.cswala.Activities.LoginActivity;
 import com.cswala.cswala.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileFragment extends Fragment {
 
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        TextView tv = view.findViewById(R.id.logout);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                firebaseAuth.signOut();
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+
+            }
+        });
+
+        return view;
     }
 }
