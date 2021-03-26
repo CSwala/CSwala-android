f103f6383c02e7f00466d27a2c15c86d4ffda281
venkatesh1226 <venkatesh1226m@gmail.com>
2021-03-26 16:28:35 +0530

    Implemented job hunt fragment

--- app/src/main/java/com/cswala/cswala/Fragments/JobHunt.java
+++ app/src/main/java/com/cswala/cswala/Fragments/JobHunt.java
@@ -1,22 +1,33 @@
 package com.cswala.cswala.Fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import com.cswala.cswala.Adapters.JobHuntViewPagerAdapter;
 import com.cswala.cswala.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class JobHunt extends Fragment {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    JobHuntViewPagerAdapter viewPagerAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_job_hunt, container, false);
+        viewPager = view.findViewById(R.id.viewPager);
+        viewPagerAdapter = new JobHuntViewPagerAdapter(getChildFragmentManager());
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout = view.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
         return view;
     }
 }
