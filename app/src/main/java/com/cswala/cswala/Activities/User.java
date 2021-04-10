fd2981fa6bcb4917d87593cacd7c858913b83b83
RashikaJoshi-777 <75016031+RashikaJoshi-777@users.noreply.github.com>
2021-04-10 13:37:27 +0530

    Update User.java

--- app/src/main/java/com/cswala/cswala/Activities/User.java
+++ app/src/main/java/com/cswala/cswala/Activities/User.java
@@ -2,7 +2,7 @@ package com.cswala.cswala.Activities;
 
 public class User {
 
-    public String email;
+    private String Name, Email, Phone, Gender, Dob, Image;
 
     public User(){
 
@@ -12,5 +12,62 @@ public class User {
 
         this.email =email;
 
+    }
+    
+     public User(String image, String name, String email, String phone, String gender, String dob) {
+        Name = name;
+        Email = email;
+        Phone = phone;
+        Gender = gender;
+        Dob = dob;
+        Image = image;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public String getDob() {
+        return Dob;
+    }
+
+    public void setDob(String dob) {
+        Dob = dob;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public void setImage(String image) {
+        Image = image;
     }
 }
