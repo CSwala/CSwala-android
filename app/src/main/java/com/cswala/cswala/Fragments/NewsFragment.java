85c77a0ed9739b6fc109103649e2eb00b8cf3ffe
Deepika Juneja <52399007+deepsjuneja@users.noreply.github.com>
2021-04-17 12:22:25 +0530

    #139 Added swipe functionality (#161)

--- app/src/main/java/com/cswala/cswala/Fragments/NewsFragment.java
+++ app/src/main/java/com/cswala/cswala/Fragments/NewsFragment.java
@@ -8,76 +8,29 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
 
+import com.cswala.cswala.Adapters.LatestNewsAndEventsPageAdapter;
 import com.cswala.cswala.R;
 import com.google.android.material.tabs.TabLayout;
 
+import org.jetbrains.annotations.NotNull;
+
 public class NewsFragment extends Fragment {
-    public static final String TAG = "HackFeed_fragment_click";
-
-    FragmentManager fragmentManager;
-
-    public NewsFragment() {
-        // Required empty public constructor
-    }
+    TabLayout newsTabLayout;
+    ViewPager newsViewPager;
+    LatestNewsAndEventsPageAdapter newsViewPagerAdapter;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_news, container, false);
+        newsViewPager = view.findViewById(R.id.newsViewPager);
 
-        final TabLayout tabLayout = view.findViewById(R.id.tab_hf);
+        newsViewPagerAdapter = new LatestNewsAndEventsPageAdapter(getChildFragmentManager());
+        newsViewPager.setAdapter(newsViewPagerAdapter);
 
-        if(savedInstanceState==null) {
-            // Set default fragment here to be set on first time app launch
-
-            tabLayout.selectTab(tabLayout.getTabAt(0));
-            fragmentManager = getParentFragmentManager();
-            LatestNewsFragment defaultFragment = new LatestNewsFragment();
-            fragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container_hackfeed, defaultFragment)
-                    .commit();
-        }
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                tab.select();
-                Fragment fragment = null;
-                if(tab.getPosition()==0){
-                    fragment = new LatestNewsFragment();
-                }else if(tab.getPosition()==1) {
-                    fragment = new NewsEventsFragment();
-                }
-
-                if(fragment!=null){
-                    fragmentManager = getParentFragmentManager();
-                    fragmentManager.beginTransaction()
-                            .replace(R.id.fragment_container_hackfeed, fragment)
-                            .commit();
-                }else{
-                    Log.e(TAG, "Error in creating fragment");
-                }
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
+        newsTabLayout = view.findViewById(R.id.newsTabs);
+        newsTabLayout.setupWithViewPager(newsViewPager);
         return view;
     }
-
 }
