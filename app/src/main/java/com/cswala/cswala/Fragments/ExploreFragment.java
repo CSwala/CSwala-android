9881dbd7859e00116647cf8edc6f45dc9274f198
ANUJAGRAWAL2000 <anujagr2001@gmail.com>
2021-05-02 10:58:33 -0700

    Add Begineer recyclerview in ExploreFragment

--- app/src/main/java/com/cswala/cswala/Fragments/ExploreFragment.java
+++ app/src/main/java/com/cswala/cswala/Fragments/ExploreFragment.java
@@ -20,13 +20,16 @@ import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cswala.cswala.Activities.TechDataActivity;
+import com.cswala.cswala.Adapters.BestPracticeAdapter;
 import com.cswala.cswala.Adapters.TechListAdapter;
 import com.cswala.cswala.Adapters.techItemClicked;
 import com.cswala.cswala.MainActivity;
+import com.cswala.cswala.Models.Begineer;
 import com.cswala.cswala.Models.TechListElement;
 import com.cswala.cswala.R;
 import com.cswala.cswala.utils.IntentHelper;
@@ -40,15 +43,21 @@ import com.google.firebase.firestore.QuerySnapshot;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ExploreFragment extends Fragment implements techItemClicked {
     RecyclerView techListView;
+    RecyclerView rvBestPractice;
     RecyclerView.LayoutManager manager;
     SearchView searchTech;
     FirebaseFirestore firestore;
     CollectionReference reference;
     TechListAdapter adapter;
+    BestPracticeAdapter bestPracticeAdapter;
+    List<Begineer> begineerList;
+    ImageView ivClose;
+    LinearLayout llbeginner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -59,15 +68,39 @@ public class ExploreFragment extends Fragment implements techItemClicked {
         firestore=FirebaseFirestore.getInstance();
         reference=firestore.collection("Dictionary");
         techListView=(RecyclerView) view.findViewById(R.id.tech_list);
+        rvBestPractice=view.findViewById(R.id.rvBestPractice);
         manager=new LinearLayoutManager(getContext());
         techListView.setLayoutManager(manager);
+        ivClose=view.findViewById(R.id.ivClose);
+        ivClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                llbeginner.setVisibility(View.GONE);
+            }
+        });
+        llbeginner=view.findViewById(R.id.llBeginner);
+        llbeginner.setVisibility(View.VISIBLE);
         LayoutAnimationController layoutAnimationController = AnimationUtils.loadLayoutAnimation(getContext(), R.anim.layout_anim_fall_down);
         techListView.setLayoutAnimation(layoutAnimationController);
         fetchData();
         searchTech=(SearchView) view.findViewById(R.id.tech_search);
         setSearchViewParameters();
+        setBeginnerOptions();
 
         return view;
+    }
+
+    private void setBeginnerOptions() {
+        begineerList=new ArrayList<>();
+        Begineer begineer=new Begineer();
+        begineer.setTitle("Best Practice");
+        begineerList.add(begineer);
+        Begineer begineer1=new Begineer();
+        begineer1.setTitle("Beginner friendly");
+        begineerList.add(begineer1);
+        bestPracticeAdapter=new BestPracticeAdapter(getContext(),begineerList);
+        rvBestPractice.setLayoutManager(new LinearLayoutManager(getContext(),RecyclerView.HORIZONTAL,false));
+        rvBestPractice.setAdapter(bestPracticeAdapter);
     }
 
     private void setSearchViewParameters() {
